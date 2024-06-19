eeb9fcdb7d01f5a648cde7f9fa1284d69d91fc84
Shiyu Chen <42055092+s3713434@users.noreply.github.com>
2024-06-19 16:43:28 +1000

    Finishing 13-random-choices

--- 13-random-choices/script.js
+++ 13-random-choices/script.js
@@ -5,6 +5,13 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+    randomSelect()
+  }
 })
 
 function createTags(input) {
@@ -19,4 +26,39 @@ function createTags(input) {
     tagEl.innerText = tag
     tagsEl.appendChild(tagEl)
   })
+}
+
+function randomSelect() {
+  console.log(123)
+  const times = 30
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    hightlightTag(randomTag)
+
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100)
+  }, 100)
+
+  setTimeout(() => {
+    clearInterval(interval)
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      hightlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function hightlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
 }
